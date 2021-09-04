881410bbb7f5667d1629a4a25d19e7c969cd1ba6
amaslan <annie@maslan.com>
2021-09-03 17:12:38 -0700

    default threshold commenting

--- dimelo/visualize.py
+++ dimelo/visualize.py
@@ -46,8 +46,8 @@ def enrich_sm_roi(
             :param bedFile: specified windows for regions of interest
             :param basemod: which basemods, currently supported options are 'A', 'CG', 'A+CG'
             :param outDir: directory to output plot
-            :param threshA: threshold for calling mA
-            :param threshC: threshold for calling mCG
+            :param threshA: threshold for calling mA; default 128
+            :param threshC: threshold for calling mCG; default 128
             :param windowSize: window size around center point of feature of interest to plot (+/-); default 1000 bp
             :param colorA: color in hex for mA
             :param colorC: color in hex for mCG
@@ -128,7 +128,7 @@ def browser_sm_roi(
     bedFileFeatures=None,
     colorA=COLOR_A,
     colorC=COLOR_C,
-    dotsize=2,
+    dotsize=4,
     static=False,
 ):
     """
@@ -139,12 +139,12 @@ def browser_sm_roi(
             :param window: formatted as for example: "chr1:1-100000"
             :param basemod: which basemods, currently supported options are 'A', 'CG', 'A+CG'
             :param outDir: directory to output plot
-            :param threshA: threshold for calling mA
-            :param threshC: threshold for calling mCG
+            :param threshA: threshold for calling mA; default 128
+            :param threshC: threshold for calling mCG; default 128
             :param bedFileFeatures: annotation to display in browser (optional); default None
             :param colorA: color in hex for mA
             :param colorC: color in hex for mCG
-            :param dotsize: size of points; default 2
+            :param dotsize: size of points; default 4
             :param static: produce pdf if True, produe html if False; default False
     Return:
             plot of single molecules centered at region of interest
