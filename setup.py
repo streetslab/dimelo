2f4294396db86a6d7cae53ba37b6fd5be4092dcf
amaslan <annie@maslan.com>
2021-09-03 17:36:18 -0700

    fix cython typo

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ REQUIRED_PACKAGES = [
     "seaborn",
     "pyranges",
     "psutil==5.8.0",
-    "cyton==0.29.24",
+    "cython==0.29.24",
 ]
 
 # Read in the project description. We define this in the README file.
