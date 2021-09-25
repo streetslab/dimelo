7dedcf09cccb28f67a091237f0df7389ae3d7ff3
amaslan <annie@maslan.com>
2021-09-24 17:08:45 -0700

    fix parsing

--- dimelo/parse_bam.py
+++ dimelo/parse_bam.py
@@ -35,9 +35,14 @@ class Methylation(object):
         self.called_sites = called_sites
 
 
+# global dictionary update
+ave_dict = {}
+
 ####################################################################################
 # extracting modified base info from bams
 ####################################################################################
+
+
 def parse_bam(
     fileName,
     sampleName,
@@ -46,6 +51,8 @@ def parse_bam(
     center=False,
     windowSize=None,
     region=None,
+    threshA=128,
+    threshC=128,
 ):
     """Create methylation object. Process windows in parallel.
     Args:
@@ -55,6 +62,8 @@ def parse_bam(
             :param basemod: which basemod, currently supported options are 'A', 'CG', 'A+CG'
             :param center: report positions with respect to reference center (+/- window size) if True or in original reference space if False
             :param windowSize: window size around center point of feature of interest to plot (+/-); only mods within this window are stored; only specify if center=True
+            :param threshA: threshold above which to call an A base methylated
+            :param threshC: threshold above which to call a C base methylated
     Return:
             dataframe with: read_name, strand, chr, position, probability, mod
     """
@@ -68,7 +77,14 @@ def parse_bam(
         num_cores = multiprocessing.cpu_count()
         meth_data = Parallel(n_jobs=num_cores)(
             delayed(parse_ont_bam_by_window)(
-                fileName, sampleName, basemod, windowSize, w, center
+                fileName,
+                sampleName,
+                basemod,
+                windowSize,
+                w,
+                center,
+                threshA,
+                threshC,
             )
             for w in windows
         )
@@ -82,12 +98,19 @@ def parse_bam(
 
     if region is not None:
         return parse_ont_bam_by_window(
-            fileName, sampleName, basemod, windowSize, region, center
+            fileName,
+            sampleName,
+            basemod,
+            windowSize,
+            region,
+            center,
+            threshA,
+            threshC,
         )
 
 
 def parse_ont_bam_by_window(
-    fileName, sampleName, basemod, windowSize, window, center
+    fileName, sampleName, basemod, windowSize, window, center, threshA, threshC
 ):
     """Create methylation object for each window.
     Args:
@@ -97,6 +120,8 @@ def parse_ont_bam_by_window(
             :param windowSize: window size around center point of feature of interest to plot (+/-); only mods within this window are stored; only applicable for center=True
             :param window: window from bed file
             :param center: report positions with respect to reference center (+/- window size) if True or in original reference space if False
+            :param threshA: threshold above which to call an A base methylated
+            :param threshC: threshold above which to call a C base methylated
     Return:
             methylation object for a given window
     """
@@ -108,7 +133,9 @@ def parse_ont_bam_by_window(
         [
             (mod, positions, probs),
             (mod2, positions2, probs2),
-        ] = get_modified_reference_positions(read, basemod, window, center)
+        ] = get_modified_reference_positions(
+            read, basemod, window, center, threshA, threshC
+        )
         for pos, prob in zip(positions, probs):
             if pos is not None:
                 if (center is True and abs(pos) <= windowSize) or (
@@ -160,13 +187,17 @@ def parse_ont_bam_by_window(
     return data_return
 
 
-def get_modified_reference_positions(read, basemod, window, center):
+def get_modified_reference_positions(
+    read, basemod, window, center, threshA, threshC
+):
     """Extract mA and mC pos & prob information for the read
     Args:
             :param read: single read from bam file
             :param basemod: which basemods, currently supported options are 'A', 'CG', 'A+CG'
             :param window: window from bed file
             :param center: report positions with respect to reference center (+/- window size) if True or in original reference space if False
+            :param threshA: threshold above which to call an A base methylated
+            :param threshC: threshold above which to call a C base methylated
     """
     if (read.has_tag("Mm")) & (";" in read.get_tag("Mm")):
         mod1 = read.get_tag("Mm").split(";")[0].split(",", 1)[0]
@@ -180,13 +211,13 @@ def get_modified_reference_positions(read, basemod, window, center):
             base2 = base
         if len(mod1_list) > 1 and (base in mod1 or base2 in mod1):
             mod1_return = get_mod_reference_positions_by_mod(
-                read, mod1, 0, window, center
+                read, mod1, 0, window, center, threshA, threshC
             )
         else:
             mod1_return = (None, [None], [None])
         if len(mod2_list) > 1 and (base in mod2 or base2 in mod2):
             mod2_return = get_mod_reference_positions_by_mod(
-                read, mod2, 1, window, center
+                read, mod2, 1, window, center, threshA, threshC
             )
             return (mod1_return, mod2_return)
         else:
@@ -195,13 +226,17 @@ def get_modified_reference_positions(read, basemod, window, center):
         return ((None, [None], [None]), (None, [None], [None]))
 
 
-def get_mod_reference_positions_by_mod(read, basemod, index, window, center):
+def get_mod_reference_positions_by_mod(
+    read, basemod, index, window, center, threshA, threshC
+):
     """Get positions and probabilities of modified bases for a single read
     Args:
             :param read: one read in bam file
             :param mod: which basemod, reported as base+x/y/m
             :param window: window from bed file
             :param center: report positions with respect to reference center (+/- window size) if True or in original reference space if False
+            :param threshA: threshold above which to call an A base methylated
+            :param threshC: threshold above which to call a C base methylated
     """
     base, mod = basemod.split("+")
     deltas = [
@@ -241,46 +276,85 @@ def get_mod_reference_positions_by_mod(read, basemod, index, window, center):
     # extract CpG sites only rather than all mC
     keep = []
     prob_keep = []
+    all_bases_index = []
     i = 0
     seq = read.get_forward_sequence()
     # deal with None for refpos from soft clipped / unaligned bases
     if "C" in basemod:
-        for m in modified_bases:
+        for b in base_index:
             if (
-                m < len(seq) - 1
+                b < len(seq) - 1
             ):  # if modified C is not the last base in the read
-                if (refpos[m] is not None) & (refpos[m + 1] is not None):
-                    if seq[m + 1] == "G":
+                if (refpos[b] is not None) & (refpos[b + 1] is not None):
+                    if seq[b + 1] == "G":
                         if (
-                            abs(refpos[m + 1] - refpos[m]) == 1
+                            abs(refpos[b + 1] - refpos[b]) == 1
                         ):  # ensure there isn't a gap
-                            keep.append(m)
-                            prob_keep.append(i)
-            i = i + 1
-    # for m6A no need to look at neighboring base; do need to remove refpos that are None
-    else:
-        for m in modified_bases:
-            if refpos[m] is not None:
-                keep.append(m)
-                prob_keep.append(i)
-            i = i + 1
+                            all_bases_index.append(
+                                b
+                            )  # add to all_bases_index whether or not modified
+                            if b in modified_bases:
+                                if probabilities[i] > threshC:
+                                    keep.append(b)
+                                    prob_keep.append(i)
+            # increment for each instance of modified base
+            if b in modified_bases:
+                i = i + 1
+    else:  # for m6A no need to look at neighboring base; do need to remove refpos that are None
+        for b in base_index:
+            if refpos[b] is not None:
+                all_bases_index.append(
+                    b
+                )  # add to all_bases_index whether or not modified
+                if b in modified_bases:
+                    if probabilities[i] > threshA:
+                        keep.append(b)
+                        prob_keep.append(i)
+            # increment for each instance of modified base
+            if b in modified_bases:
+                i = i + 1
     # adjust position to be centered at 0 at the center of the motif; round in case is at 0.5
     # add returning base_index for plotting mod/base_abundance
     if center is True:
         if window.strand == "+":
-            refpos_adjusted = np.array(refpos[keep]) - round(
+            refpos_mod_adjusted = np.array(refpos[keep]) - round(
+                ((window.end - window.begin) / 2 + window.begin)
+            )
+            refpos_total_adjusted = np.array(refpos[all_bases_index]) - round(
                 ((window.end - window.begin) / 2 + window.begin)
             )
         if window.strand == "-":
-            refpos_adjusted = -1 * (
+            refpos_mod_adjusted = -1 * (
                 np.array(refpos[keep])
                 - round(((window.end - window.begin) / 2 + window.begin))
             )
-        return (basemod, refpos_adjusted, probabilities[prob_keep])
+            refpos_total_adjusted = -1 * (
+                np.array(refpos[all_bases_index])
+                - round(((window.end - window.begin) / 2 + window.begin))
+            )
+        update_ave_dict(refpos_mod_adjusted, refpos_total_adjusted, basemod)
+        return (basemod, refpos_mod_adjusted, probabilities[prob_keep])
     else:
+        update_ave_dict(refpos[keep], refpos[all_bases_index])
         return (basemod, np.array(refpos[keep]), probabilities[prob_keep])
 
 
-def main():
-    # TODO add argument parsing
-    print("main")
+def update_ave_dict(refpos_mod, refpos_total, basemod):
+    """
+    {pos:modification: [methylated_bases, total_bases]}
+    """
+    for pos in refpos_total:
+        # key is pos:mod
+        key = str(refpos_total) + ":" + basemod
+        # increment if the key is already in the dictionary
+        if key in ave_dict:
+            ave_dict[key][1] += 1
+            # increment modified cout if in modified list
+            if pos in refpos_mod:
+                ave_dict[key][0] += 1
+        # add value of 1 if the key is not already in the dictionary
+        else:
+            if pos in refpos_mod:
+                ave_dict[key] = [1, 1]
+            else:
+                ave_dict[key] = [0, 1]
