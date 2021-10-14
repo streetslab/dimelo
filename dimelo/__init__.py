093f40aea4a1810235624a0a9ba7a193d5d92488
amaslan <annie@maslan.com>
2021-10-13 17:47:07 -0700

    add import of enrich overlay function

--- dimelo/__init__.py
+++ dimelo/__init__.py
@@ -9,4 +9,5 @@ dimelo allows you to perform quality control and plot modified bases from bam fi
 """
 from .browser_sm_roi import browser_sm_roi
 from .enrich_sm_roi import enrich_sm_roi
+from .enrich_sm_roi_overlay import enrich_sm_roi_overlay
 from .parse_bam import parse_bam
