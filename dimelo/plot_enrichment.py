22730ec561788156090f31c5467dc8cfa33f9d3d
amaslan <annie@maslan.com>
2022-01-26 10:01:13 -0800

    function for enrichment barplots

--- dimelo/plot_enrichment.py
+++ dimelo/plot_enrichment.py
@@ -1,6 +1,197 @@
-def plot_enrichment():
-    # TODO add barchart - be sure to have cores as optional argument
-    print("enrichment")
+import multiprocessing
+import sqlite3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from dimelo.parse_bam import parse_bam
+
+COLOR_LIST = ["#2D1E2F", "#A9E5BB", "#610345", "#559CAD", "#5E747F"]
+
+
+def plot_enrichment(
+    fileNames,
+    sampleNames,
+    bedFiles,
+    basemod,
+    outDir,
+    threshA=129,
+    threshC=129,
+    colors=COLOR_LIST,
+    cores=None,
+):
+    """Create barplots showing modified/total bases in region(s) of interest for given sample(s).
+    Args:
+            :param fileNames: name(s) of bam file with Mm and Ml tags
+            :param sampleNames: name(s) of sample for output file name labelling
+            :param bedFiles: specified windows for region(s) of interest
+            :param basemod: which basemods, currently supported options are 'A', 'CG'; only valid to look at one type of mod
+            :param outDir: directory to output plot
+            :param threshA: threshold for calling mA; default 129
+            :param threshC: threshold for calling mCG; default 129
+            :param colors: color list in hex for overlay; default is ["#2D1E2F", "#A9E5BB", "#610345", "#559CAD", "#5E747F"]
+    Return:
+            barplot of modified/total bases
+    """
+
+    # default number of cores is max available
+    cores_avail = multiprocessing.cpu_count()
+    if cores is None:
+        num_cores = cores_avail
+    else:
+        # if more than available cores is specified, process with available cores
+        if cores > cores_avail:
+            num_cores = cores_avail
+        else:
+            num_cores = cores
+
+    # A+CG is not valid; only valid to look at one type of mod
+    if (basemod != "A") and (basemod != "CG"):
+        print("valid basemods are A or CG")
+        return
+
+    # if  single bam file rather than list is entered, convert to list
+    if type(fileNames) != list:
+        fileNames = [fileNames]
+    # if single sample name rather than list is entered, convert to list
+    if type(sampleNames) != list:
+        sampleNames = [sampleNames]
+    # if single bed file rather than list is entered, convert to list
+    if type(bedFiles) != list:
+        bedFiles = [bedFiles]
+
+    # extract counts and create barplots
+    # get average across all bases for regions defined in the bed file
+    columns = ["fileName", "bedFile", "sampleName", "fractionMethylated"]
+    data = []
+    if len(fileNames) > 1 or len(bedFiles) > 1:
+        if len(fileNames) > 1:
+            if len(bedFiles) > 1:
+                print(
+                    "only a single region file can be used when analyzing multiple bam files"
+                )
+                return
+            for f, n in zip(fileNames, sampleNames):
+                values = get_counts(
+                    f,
+                    n,
+                    bedFiles[0],
+                    basemod,
+                    outDir,
+                    threshA,
+                    threshC,
+                    num_cores,
+                )
+                zipped = zip(columns, values)
+                a_dictionary = dict(zipped)
+                data.append(a_dictionary)
+        if len(bedFiles) > 1:
+            if len(fileNames) > 1:
+                print(
+                    "only a single bam file can be used when analyzing multiple bed file regions"
+                )
+                return
+            for b, n in zip(bedFiles, sampleNames):
+                values = get_counts(
+                    fileNames[0],
+                    n,
+                    b,
+                    basemod,
+                    outDir,
+                    threshA,
+                    threshC,
+                    num_cores,
+                )
+                zipped = zip(columns, values)
+                a_dictionary = dict(zipped)
+                data.append(a_dictionary)
+
+    # allow for barplot for single file and region
+    if (len(fileNames) == 1) and (len(bedFiles) == 1):
+        values = get_counts(
+            fileNames[0],
+            sampleNames[0],
+            bedFiles[0],
+            basemod,
+            outDir,
+            threshA,
+            threshC,
+            num_cores,
+        )
+        zipped = zip(columns, values)
+        a_dictionary = dict(zipped)
+        data.append(a_dictionary)
+
+    df = pd.DataFrame(data)
+    # draw from aggregate to calculate modified/total in region of interest
+    if len(fileNames) == 1:
+        title = "sample_" + fileNames[0].split("/")[-1]
+    if len(bedFiles) == 1:
+        title = "region_" + bedFiles[0].split("/")[-1]
+    if (len(fileNames) == 1) and (len(bedFiles) == 1):
+        title = (
+            "sample_"
+            + fileNames[0].split("/")[-1]
+            + "_region_"
+            + bedFiles[0].split("/")[-1]
+        )
+    plot_barchart(df, basemod, outDir, colors, title)
+
+
+def get_counts(
+    fileName,
+    sampleName,
+    bedFile,
+    basemod,
+    outDir,
+    threshA,
+    threshC,
+    num_cores,
+):
+    # parse_bam for files / regions
+    parse_bam(
+        fileName,
+        sampleName,
+        outDir,
+        bedFile,
+        basemod,
+        threshA=threshA,
+        threshC=threshC,
+        cores=num_cores,
+    )
+
+    # get aggregate counts
+    aggregate_counts = pd.read_sql(
+        "SELECT * from methylationAggregate_" + sampleName,
+        sqlite3.connect(
+            outDir + "/" + fileName.split("/")[-1].replace(".bam", "") + ".db"
+        ),
+    )
+    methylated_bases = aggregate_counts["methylated_bases"].sum()
+    total_bases = aggregate_counts["total_bases"].sum()
+    if total_bases == 0:
+        fractionMethylated = 0
+    else:
+        fractionMethylated = methylated_bases / total_bases
+
+    return [fileName, bedFile, sampleName, fractionMethylated]
+
+
+def plot_barchart(data, basemod, outDir, colors, title):
+    """
+    x-axis: sample or region
+    y-axis: fraction methylated bases
+    """
+    fig, ax1 = plt.subplots()
+    plt.bar("sampleName", "fractionMethylated", data=data, color=colors)
+    sns.despine(fig)
+    plt.ylabel("fraction methylated bases")
+    plt.xlabel("")
+    plt.savefig(
+        outDir + "/" + title + "_" + basemod + "_enrichment_barplot.png",
+        dpi=300,
+    )
 
 
 def main():
