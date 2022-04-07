108ca2d48a34400f43e0738cc14f06ec7d3a141c
amaslan <annie@maslan.com>
2022-04-07 15:03:19 -0700

    add methplotlib license for plot_browser

--- dimelo/plot_browser.py
+++ dimelo/plot_browser.py
@@ -8,6 +8,18 @@ plot_browser module
 
 plot_browser plots single molecules with colored base modifications in region of interest
 
+
+Portions of code adapted from methplotlib:
+Copyright (c) 2018 Wouter De Coster
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
 """
 
 # code adapted from methplotlib
