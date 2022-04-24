1be322a5385a86dbb9b7ca6f6bf45e6860c7f59e
thekugelmeister <jeremy.i.marcus@gmail.com>
2022-04-24 13:30:40 -0700

    first pass at parse_bam db test

--- dimelo/test/dimelo_test.py
+++ dimelo/test/dimelo_test.py
@@ -1,5 +1,8 @@
 import unittest
+import subprocess
+from pathlib import Path
 
+import dimelo as dm
 from dimelo.test import DiMeLoTestCase
 
 # import pandas as pd
@@ -11,73 +14,105 @@ from dimelo.test import DiMeLoTestCase
 # )
 
 
-class TestDiMeLo(DiMeLoTestCase):
-    def test_parse_ont_bam_number(self):
-        """
-        This function calls helper functions:
-        - parse_ont_bam_by_window
-        - get_modified_reference_positions
-        - get_mod_reference_positions_by_mod
-        Therefore, tests all four total functions in parse_bam.py.
-        Tests under various conditions:
-        - Bam: megalodon or guppy+winnowmap
-        - Basemod: A, CG or A+CG
-        - Center: True or False
-        For reference, read names extracted are:
-        'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
-        '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
-        '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
-        This is testing correct number of bases are extracted
-        """
-        # all_tests = create_methylation_objects()
+class TestParseBam(DiMeLoTestCase):
+    def setUp(self):
+        self.outDir = Path(self.tmpDir())
+        
+    def test_parse_bam_bedFile(self):
+        # TODO: Is this a reasonable way to specify input files? Where is this intended to be run from?
+        dm.parse_bam(fileName="dimelo/test/data/mod_mappings_subset.bam",
+                     sampleName="test",
+                     outDir=str(self.outDir),
+                     bedFile="dimelo/test/data/test.bed",
+                     basemod="A+CG",
+                     center=True,
+                     windowSize=500,
+                     threshA=190,
+                     threshC=190,
+                     extractAllBases=False,
+                     cores=1
+        )
+        # TODO: When implemented elsewhere, replace this explicit database path with a modular call
+        database_path = self.outDir / "mod_mappings_subset.db"
+        # Check whether database contents are the same as expected, using sqlite3 .sha3sum command
+        db_hash_output = subprocess.run(
+            ["sqlite3", database_path, ".sha3sum"],
+            capture_output=True
+        )
+        self.assertEqual(
+            db_hash_output.stdout,
+            b"b38a246e912e4e160d840a1650108c624774175b35e6afc98d974805\n"
+        )
 
-        # test extracting correct number of bases
-        # placeholder for now
-        assert 1 == 1
 
-    def test_parse_ont_bam_position(self):
-        """
-        This function calls helper functions:
-        - parse_ont_bam_by_window
-        - get_modified_reference_positions
-        - get_mod_reference_positions_by_mod
-        Therefore, tests all four total functions in parse_bam.py.
-        Tests under various conditions:
-        - Bam: megalodon or guppy+winnowmap
-        - Basemod: A, CG or A+CG
-        - Center: True or False
-        For reference, read names extracted are:
-        'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
-        '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
-        '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
-        Test with a forward read and a reverse read.
-        This is testing correct positions of bases are extracted for a test read
-        """
-        # placeholder for now
-        assert 1 == 1
+# class TestDiMeLo(DiMeLoTestCase):
+    
+    # def test_parse_ont_bam_number(self):
+    #     """
+    #     This function calls helper functions:
+    #     - parse_ont_bam_by_window
+    #     - get_modified_reference_positions
+    #     - get_mod_reference_positions_by_mod
+    #     Therefore, tests all four total functions in parse_bam.py.
+    #     Tests under various conditions:
+    #     - Bam: megalodon or guppy+winnowmap
+    #     - Basemod: A, CG or A+CG
+    #     - Center: True or False
+    #     For reference, read names extracted are:
+    #     'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
+    #     '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
+    #     '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
+    #     This is testing correct number of bases are extracted
+    #     """
+    #     # all_tests = create_methylation_objects()
 
-    # test qualities parsed correctly
-    def test_parse_ont_bam_probability(self):
-        """
-        This function calls helper functions:
-        - parse_ont_bam_by_window
-        - get_modified_reference_positions
-        - get_mod_reference_positions_by_mod
-        Therefore, tests all four total functions in parse_bam.py.
-        Tests under various conditions:
-        - Bam: megalodon or guppy+winnowmap
-        - Basemod: A, CG or A+CG
-        - Center: True or False
-        For reference, read names extracted are:
-        'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
-        '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
-        '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
-        Test with a forward read and a reverse read.
-        This is testing correct probabilities of methylation are extracted for a test read
-        """
+    #     # test extracting correct number of bases
+    #     # placeholder for now
+    #     assert 1 == 1
 
-        # placeholder for now
-        assert 1 == 1
+    # def test_parse_ont_bam_position(self):
+    #     """
+    #     This function calls helper functions:
+    #     - parse_ont_bam_by_window
+    #     - get_modified_reference_positions
+    #     - get_mod_reference_positions_by_mod
+    #     Therefore, tests all four total functions in parse_bam.py.
+    #     Tests under various conditions:
+    #     - Bam: megalodon or guppy+winnowmap
+    #     - Basemod: A, CG or A+CG
+    #     - Center: True or False
+    #     For reference, read names extracted are:
+    #     'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
+    #     '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
+    #     '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
+    #     Test with a forward read and a reverse read.
+    #     This is testing correct positions of bases are extracted for a test read
+    #     """
+    #     # placeholder for now
+    #     assert 1 == 1
+
+    # # test qualities parsed correctly
+    # def test_parse_ont_bam_probability(self):
+    #     """
+    #     This function calls helper functions:
+    #     - parse_ont_bam_by_window
+    #     - get_modified_reference_positions
+    #     - get_mod_reference_positions_by_mod
+    #     Therefore, tests all four total functions in parse_bam.py.
+    #     Tests under various conditions:
+    #     - Bam: megalodon or guppy+winnowmap
+    #     - Basemod: A, CG or A+CG
+    #     - Center: True or False
+    #     For reference, read names extracted are:
+    #     'f2f51b7c-7818-4771-9101-258a6ee8ecb4',
+    #     '35dbe94b-025a-4d0c-94e5-724bb2bdba0d',
+    #     '360915ea-cab1-4bf4-85ea-2eaa3e79e5a8'
+    #     Test with a forward read and a reverse read.
+    #     This is testing correct probabilities of methylation are extracted for a test read
+    #     """
+
+    #     # placeholder for now
+    #     assert 1 == 1
 
     # TODO add md5sum check for plot
     # def test_enrich_sm_roi(self):
