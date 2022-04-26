96542d47e73bcee1ccc851c14195d716af9c34ee
thekugelmeister <jeremy.i.marcus@gmail.com>
2022-04-26 16:49:48 -0700

    testing tempfile compatibility with Travis CI

--- dimelo/test/__init__.py
+++ dimelo/test/__init__.py
@@ -1,6 +1,7 @@
 import tempfile
 import unittest
 from pathlib import Path
+import shutil
 
 
 class DiMeLoTestCase(unittest.TestCase):
@@ -11,12 +12,14 @@ class DiMeLoTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls._outDir = tempfile.TemporaryDirectory()
-        cls.outDir = Path(cls._outDir.name)
+        # cls._outDir = tempfile.TemporaryDirectory()
+        # cls.outDir = Path(cls._outDir.name)
+        cls.outDir = Path('dimelo/dimelo_test')
 
     @classmethod
     def tearDownClass(cls):
-        cls._outDir.cleanup()
+        # cls._outDir.cleanup()
+        shutil.rmtree(cls.outDir)
 
     # def tmpFile(self):
     #     tempFile = tempfile.NamedTemporaryFile(delete=True)
