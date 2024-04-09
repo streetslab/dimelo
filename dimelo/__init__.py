b263d8da172f39723614287e277b9a224c665b10
Jeremy Marcus <jeremy.i.marcus@gmail.com>
2024-04-08 17:20:13 -0700

    Adding __all__ to more explicitly expose submodules

--- dimelo/__init__.py
+++ dimelo/__init__.py
@@ -1,1 +1,2 @@
 from . import parse_bam, load_processed, plot_enrichment, plot_enrichment_profile, plot_reads
+__all__ = ['parse_bam', 'load_processed', 'plot_enrichment', 'plot_enrichment_profile', 'plot_reads']
